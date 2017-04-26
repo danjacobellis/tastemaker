dbbb2bfd2b37e06dbfeeb31f54c4ce4eea0cdfbb
Dan Jacobellis <danjacobellis@gmail.com>
2017-04-26 15:02:26 -0500

    analyze most frequent categories

--- example.py
+++ example.py
@@ -8,6 +8,8 @@ input_filename = 'recipes' + os.sep + '10000.mmf'
 
 recipes, categories, ingredients = mmf.read_mmf_file(input_filename)
 
+
+#--------------------------------ingredients-----------------------------------
 all_ingredients = []
 for ingredient_list in ingredients:
     for ingredient in ingredient_list:
@@ -15,21 +17,18 @@ for ingredient_list in ingredients:
         
 for index, ingredient in enumerate(all_ingredients):
     all_ingredients[index] = ingredient.replace(" ","_")
-#    for match in re.finditer(' ', ingredient):
-#        ingredient[ma
-
-    
+ 
 from sklearn.feature_extraction.text import CountVectorizer
-count_vect = CountVectorizer()
-term_doc_matrix = count_vect.fit_transform(all_ingredients).toarray()
-inv_trans = count_vect.inverse_transform(term_doc_matrix)
+count_vect_ing = CountVectorizer()
+term_doc_ing = count_vect_ing.fit_transform(all_ingredients).toarray()
+inv_trans = count_vect_ing.inverse_transform(term_doc_ing)
 
-num_unique = np.shape(term_doc_matrix)[1]
-num_ingredients = np.shape(term_doc_matrix)[0]
-print("there are", num_unique, "unique ingredients out of",num_ingredients)
+num_unique_ing = np.shape(term_doc_ing)[1]
+num_ing = np.shape(term_doc_ing)[0]
+print("there are", num_unique_ing, "unique ingredients out of",num_ing)
 
 import matplotlib.pyplot as plt
-ingredient_freq = np.sum(term_doc_matrix, axis=0)
+ingredient_freq = np.sum(term_doc_ing, axis=0)
 sorted_ingredient_freqs = np.flipud(np.argsort(ingredient_freq))
 
 fig = plt.figure()
@@ -43,13 +42,55 @@ plt.ylabel('Number of Occurances of Ingredient')
 
 num_keep = 25
 top_ind = sorted_ingredient_freqs[0:num_keep]
-keep_term_doc = np.zeros((num_keep,num_unique))
+keep_term_doc = np.zeros((num_keep,num_unique_ing))
 for ind, val in enumerate(top_ind):
     keep_term_doc[ind,val] = 1
-labels = count_vect.inverse_transform(keep_term_doc)
+labels = count_vect_ing.inverse_transform(keep_term_doc)
 freqs = ingredient_freq[top_ind]
 
-print("Most common ingredients:", labels,freqs)
+print("Most common ingredients:\n", labels)
+print("Number of occurances:\n",freqs)
+
+#--------------------------------ingredients-----------------------------------
+all_categories = []
+for category_list in categories:
+    for category in category_list:
+        all_categories.append(category)
+        
+for index, category in enumerate(all_categories):
+    all_categories[index] = category.replace(" ","_")
+ 
+from sklearn.feature_extraction.text import CountVectorizer
+count_vect_cat = CountVectorizer()
+term_doc_cat = count_vect_cat.fit_transform(all_categories).toarray()
+inv_trans = count_vect_cat.inverse_transform(term_doc_cat)
+
+num_unique_cat = np.shape(term_doc_cat)[1]
+num_cat= np.shape(term_doc_cat)[0]
+print("there are", num_unique_cat, "unique categories out of",num_cat)
+
+import matplotlib.pyplot as plt
+category_freq = np.sum(term_doc_cat, axis=0)
+sorted_category_freqs = np.flipud(np.argsort(category_freq))
+
+fig = plt.figure()
+fig.add_axes()
+ax1 = plt.gca()
+ax1.set_yscale('log')
+ax1.set_xscale('log')
+plt1 = plt.plot(category_freq[sorted_category_freqs])
+plt.xlabel('Category Index')
+plt.ylabel('Number of Occurances of Category')
+
+num_keep = 25
+top_ind = sorted_category_freqs[0:num_keep]
+keep_term_doc = np.zeros((num_keep,num_unique_cat))
+for ind, val in enumerate(top_ind):
+    keep_term_doc[ind,val] = 1
+labels = count_vect_cat.inverse_transform(keep_term_doc)
+freqs = category_freq[top_ind]
+
+print("Most common categories:", labels,freqs)
 
 
 
