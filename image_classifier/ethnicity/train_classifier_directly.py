be38f0c678b2fcdb5d9e6d9335f307d4b6a693df
Dan Jacobellis <danjacobellis@gmail.com>
2017-05-03 00:01:10 -0500

    stole kevin's script to delete bad files

--- image_classifier/ethnicity/train_classifier_directly.py
+++ image_classifier/ethnicity/train_classifier_directly.py
@@ -1,34 +1,18 @@
 import os
+import imghdr
 
 image_dir = "/home/dan/Desktop/scraped/ethnicity"
-
-bad_files = [image_dir+"/hungarian/30.jpg",
-             image_dir+"/dairy/5.jpg",
-             image_dir+"/dairy/80.jpg",
-             image_dir+"/dairy/41.jpg",
-             image_dir+"/dairy/30.jpg",
-             image_dir+"/dairy/61.jpg",
-             image_dir+"/dairy/18.jpg",
-             image_dir+"/dairy/60.jpg",
-             image_dir+"/dairy/31.jpg",
-             image_dir+"/norwegian/68.jpg",
-             image_dir+"/norwegian/13.jpg",
-             image_dir+"/norwegian/46.jpg",
-             image_dir+"/norwegian/79.jpg",
-             image_dir+"/norwegian/37.jpg",
-             image_dir+"/norwegian/43.jpg",
-             image_dir+"/norwegian/78.jpg",
-             image_dir+"/appetizers/1.jpg",
-             image_dir+"/appetizers/44.jpg",
-             image_dir+"/appetizers/84.jpg",
-             image_dir+"/appetizers/99.jpg",
-             image_dir+"/american/20.jpg",
-             image_dir+"/american/32.jpg",
-             image_dir+"/american/75.jpg",
-             image_dir+"/southern/37.jpg",
-             image_dir+"/southern/36.jpg",
-             image_dir+"/southern/78.jpg",
-             image_dir+"/african/86.jpg",]
+dirs = os.listdir(image_dir)
+bad_files = [];
+for directory in dirs:
+    directory = image_dir + os.sep + directory
+    files = os.listdir(directory)
+    for file in files:
+        file = directory + os.sep + file
+        what = (imghdr.what(file))
+        if what != 'jpeg':
+            print(file, "is invalid")
+            bad_files.append(file)
 
 for file in bad_files:
     if os.path.isfile(file):
