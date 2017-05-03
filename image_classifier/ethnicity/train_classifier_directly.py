9dc026ea3cd66b77090493b526afcc40be08376b
Dan Jacobellis <danjacobellis@gmail.com>
2017-05-03 00:13:54 -0500

    changed output dir from tmp to cwd

--- image_classifier/ethnicity/train_classifier_directly.py
+++ image_classifier/ethnicity/train_classifier_directly.py
@@ -21,7 +21,16 @@ for file in bad_files:
     else:
         print(file, " does not exist")
 
+output_graph = os.getcwd() + os.sep + 'output_graph.pb'
+output_labels = os.getcwd() + os.sep + 'output_labels.txt'
+summaries_dir = os.getcwd() + os.sep + 'retrain_logs'
+bottleneck_dir = os.getcwd() + os.sep + 'bottleneck'
+
 cmd_str = "python retrain.py"
 cmd_str = cmd_str + " --image_dir " + image_dir
+cmd_str = cmd_str + " --output_graph " + output_graph
+cmd_str = cmd_str + " --output_labels " + output_labels
+cmd_str = cmd_str + " --summaries_dir " + summaries_dir
+cmd_str = cmd_str + " --bottleneck_dir " + bottleneck_dir
 
 os.system(cmd_str)
